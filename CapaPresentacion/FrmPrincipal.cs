e987afa1b00ba8b9a0a45552a1c43201b2c794f3
Abraham Estrella <Abraham Estrella@DESKTOP-5IP02MR>
2020-08-09 13:29:18 -0400

    Se Modifaron algunos formularios

---
+++ CapaPresentacion/FrmPrincipal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        public void Pantalla()
+        {
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            Pantalla();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+       
+    
+
+
+    }
+}
---
+++ CapaPresentacion/txt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PlaceholderTextBox;
+
+namespace CapaPresentacion
+{
+    public partial class txt : TextBox
+    {
+        public txt()
+        {
+            InitializeComponent();
+            this.AutoSize = false;
+        }
+
+        public txt(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
